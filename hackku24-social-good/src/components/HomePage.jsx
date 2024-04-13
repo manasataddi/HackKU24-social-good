c644e7328ed132178991ab5edcca327c10651f9f
manasataddi <90788139+manasataddi@users.noreply.github.com>
2024-04-13 17:40:08 -0500

    main page changes

--- hackku24-social-good/src/components/HomePage.jsx
+++ hackku24-social-good/src/components/HomePage.jsx
@@ -6,15 +6,20 @@ import { colors } from '@mui/material';
 
 function HomePage() {
     return (
-        <div className='maintext'>            
+        <div className='maintext'>
+            <div className='line1'>           
             <div><h1>Food For All</h1></div>
+             </div>  
+        
             <div><p>"Food For All" a web platform that bridges the gap between restaurants with surplus food and those in need, transforming potential waste into meals for the underprivileged. It’s a movement towards social good, ensuring every leftover dish finds a place at someone’s table, reducing food waste while feeding hope and dignity within the community.</p></div>
-            <div className='button'>
+           <div> 
+            <img src={"https://png.pngtree.com/png-vector/20221024/ourmid/pngtree-tiny-people-standing-near-box-of-donation-food-for-delivery-png-image_6346685.png"}/> 
+           </div> 
                 <button className='havefood'> I Have Food</button>
-                <button className='wantfood'> I want Foods</button>                
+                <button className='wantfood'> I want Food</button>                
             </div>           
-        </div>
+        
     )
-}
+}   
 
 export default HomePage;
