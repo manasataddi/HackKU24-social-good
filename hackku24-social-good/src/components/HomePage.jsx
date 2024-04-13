46b1eca5607529098fd09f8e6d90976c666016da
manasataddi <90788139+manasataddi@users.noreply.github.com>
2024-04-13 14:15:54 -0500

    Added styiling to homepage

--- hackku24-social-good/src/components/HomePage.jsx
+++ hackku24-social-good/src/components/HomePage.jsx
@@ -1,30 +1,18 @@
 import React from 'react'
 import Box from '@mui/material/Box';
+import '../home.css';
 import { colors } from '@mui/material';
 
 
 function HomePage() {
     return (
-        <div>
-            
-            <div><h1 className='titleHP'></h1>Food</div>
-
-            <Box height={200}
-                className="descriptionHomePage"
-                my={4}
-                display="flex"
-                alignItems="center"
-                gap={4}
-                p={2}
-                sx={
-                    { 
-                        border: '2px solid grey',
-                        bgcolor: 'primary.dark',
-                        
-                    }
-                }>
-                Welcome to food distribution platform. We distribute food.
-            </Box>
+        <div className='maintext'>            
+            <div><h1>Food For All</h1></div>
+            <div><p>"Food For All" a web platform that bridges the gap between restaurants with surplus food and those in need, transforming potential waste into meals for the underprivileged. It’s a movement towards social good, ensuring every leftover dish finds a place at someone’s table, reducing food waste while feeding hope and dignity within the community.</p></div>
+            <div className='button'>
+                <button className='havefood'> I Have Food</button>
+                <button className='wantfood'> I want Foods</button>                
+            </div>           
         </div>
     )
 }
